f54780041339ba6885758a1c02e828128606ac68
Olamide <olire96@gmail.com>
2023-08-08 22:50:16 +0100

    made modifications for pep8

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -9,8 +9,8 @@ class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """returns False"""
         if (path is None
-            or excluded_paths is None 
-            or len(excluded_paths) == 0):
+           or excluded_paths is None
+           or len(excluded_paths) == 0):
             return True
         if path in excluded_paths or path + '/' in excluded_paths:
             return False
