4d802fb595b4156511fb7f2c4063b84c3f95aa67
Olamide <olire96@gmail.com>
2023-08-08 20:46:02 +0100

    Third task

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -30,3 +30,9 @@ def stats() -> str:
 def raise_unauthorized() -> None:
     """Raise unauthorized"""
     abort(401)
+
+
+@app_views.route('/forbidden/', strict_slashes=False)
+def raise_forbidden() -> None:
+    """Raise a 403 error with abort"""
+    abort(403)
