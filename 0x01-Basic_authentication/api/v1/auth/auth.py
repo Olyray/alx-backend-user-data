a2f3565fb5e9fdcc4fbe548c287b0327c598c091
Olamide <olire96@gmail.com>
2023-08-08 22:46:21 +0100

    Task four

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -8,7 +8,13 @@ class Auth:
     """The class for API authentication"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """returns False"""
-        return False
+        if (path is None
+            or excluded_paths is None 
+            or len(excluded_paths) == 0):
+            return True
+        if path in excluded_paths or path + '/' in excluded_paths:
+            return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """returns None"""
