ebbef8255467ef0946d2fe6775b66f915b49448f
Olamide <olire96@gmail.com>
2023-08-09 21:18:14 +0100

    Modified a function

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -50,8 +50,8 @@ class BasicAuth(Auth):
             or ':' not in decoded_base64_authorization_header
            ):
             return None, None
-        name, email = decoded_base64_authorization_header.split(':')
-        return name, email
+        email, password = decoded_base64_authorization_header.split(':', 1)
+        return email, password
 
     def user_object_from_credentials(
             self,
