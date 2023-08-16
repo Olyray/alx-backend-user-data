efdc87043acd1601623b165a7e9fbdd6bd20eaf8
Olamide <olire96@gmail.com>
2023-08-16 19:48:19 +0100

    Task 11 and 12

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -6,6 +6,7 @@ from db import DB
 from user import User
 from sqlalchemy.orm.exc import NoResultFound
 from uuid import uuid4
+from typing import Union
 
 
 def _hash_password(password: str) -> bytes:
@@ -53,5 +54,13 @@ class Auth:
             user_uuid = _generate_uuid()
             self._db.update_user(found_user.id, session_id=user_uuid)
             return user_uuid
+        except Exception:
+            return None
+
+    def get_user_from_session_id(self, session_id: str) -> Union[User, None]:
+        """Gets a user from the session_id"""
+        try:
+            gotten_user = self._db.find_user_by(session_id=session_id)
+            return gotten_user
         except Exception:
             return None
