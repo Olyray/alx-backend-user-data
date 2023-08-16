538a24d6d9565d331c43b3b468129c947f5a3803
Olamide <olire96@gmail.com>
2023-08-16 16:52:04 +0100

    Update db.py

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,22 +1,22 @@
 #!/usr/bin/env python3
-"""DB module.
+"""DB module
 """
 from sqlalchemy import create_engine, tuple_
-from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.orm.session import Session
-
-from user import Base, User
+from typing import TypeVar
+from user import User
+from sqlalchemy.exc import NoResultFound, InvalidRequestError
+from user import Base
 
 
 class DB:
-    """DB class.
+    """DB class
     """
 
     def __init__(self) -> None:
-        """Initialize a new DB instance.
+        """Initialize a new DB instance
         """
         self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
@@ -25,7 +25,7 @@ class DB:
 
     @property
     def _session(self) -> Session:
-        """Memoized session object.
+        """Memoized session object
         """
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
@@ -33,8 +33,7 @@ class DB:
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """Adds a new user to the database.
-        """
+        """Adds user to the database"""
         try:
             new_user = User(email=email, hashed_password=hashed_password)
             self._session.add(new_user)
@@ -45,8 +44,7 @@ class DB:
         return new_user
 
     def find_user_by(self, **kwargs) -> User:
-        """Finds a user based on a set of filters.
-        """
+        """Finds a user using keyword arguments"""
         fields, values = [], []
         for key, value in kwargs.items():
             if hasattr(User, key):
