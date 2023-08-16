8add2618ba5d9969da6df48dcefeec1edb1267f1
Olamide <olire96@gmail.com>
2023-08-16 19:55:23 +0100

    Added the destroy_session

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -64,3 +64,7 @@ class Auth:
             return gotten_user
         except Exception:
             return None
+
+    def destroy_session(self, user_id: int) -> None:
+        """Ends a user's session"""
+        return self._db.update_user(user_id, session_id=None)
